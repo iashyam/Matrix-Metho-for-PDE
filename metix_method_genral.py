38779cdd68697abbfbcf2995bd2348e36dc772dc
Shyam <shyam10kwd@gmail.com>
2021-04-12 09:52:02 +0530

    chnafe

--- metix_method_genral.py
+++ metix_method_genral.py
@@ -7,10 +7,10 @@ import math
 #<== Constants ==>
 h = 1 #plank constant
 m = 1 #mass of the object
-v0 = 1 #barrier height
-a = 1
+v0 = 0.3 #barrier height
+# a = 1
 i = 1j #defining iota 
-E = np.linspace(0, 2, 200)
+E = np.linspace(0, 0.8, 400)
 
 
 Rs = []  # list of reflecing and trasmattting constant
@@ -20,7 +20,21 @@ T_th = []
 
 #List barriers as tuple if starting point, endpoint 
 # we are doing this because barrier height is same for all
-barriers = [(0,a), (2*a, 3*a), (5*a, 7*a)]
+barriers = [(4,8),(13,17)]
+
+
+# def get_barriers(number, width, distance):
+#     barriers = [(0, width)]
+#     for n in range(1, number):
+#         barriers.append((0+n*distance, width+n*distance))
+#     return barriers
+
+# v0 = eval(input('Enter the barrier height: '))
+# number = eval(input("Enter number of potential: "))
+# width = eval(input("Enter the width of one barrier: "))
+# distance = eval(input('Enter the distance between two barriers: '))
+# barriers = get_barriers(number, width, distance)
+
 
 
 def scatterinMatrix(k1, k2):
@@ -34,34 +48,11 @@ def scatterinMatrix(k1, k2):
 
 def progogationMatrix(a, k):
 
-    return np.array([[cmath.exp(i*k*a), 0], [0, cmath.exp(-i*k*a)]], dtype=complex)
-
-
-# def matrix_method(k1,k2,k3, n):
-#     a,b = barriers[0]
-#     c,d = barriers[1]
-#     d21 = scatterinMatrix(k1, k2)
-#     d32 = scatterinMatrix(k2, k3)
-
-#     p1 = progogationMatrix(a, k1)
-#     p2 = progogationMatrix(b, k2)
-#     p3 = progogationMatrix(c, k1)
-#     p4 = progogationMatrix(d, k2)
-
-
-#     m1 = np.matmul(d21, p3)
-#     m2 = np.matmul(d32, p4)
-#     m = np.matmul(m2,m1)
-
-#     M1 = np.matmul(d21, p1)h
-#     M2 = np.matmul(d32, p2)
-#     M =  np.matmul(M2, M1)
-
-#     return np.matmul(m, M)
+    return np.array([[cmath.exp(i*k*a),0], [0,cmath.exp(-i*k*a)]], dtype=complex)
 
 
 '''
-The total effect of two metrices are basically 
+The total effect of two metrices are
 (d3<-2)(P(a,k)) times this metrix for one potential box
 '''
 def matrix_method(k1,k2,k3, n):
@@ -73,17 +64,27 @@ def matrix_method(k1,k2,k3, n):
     p1 = progogationMatrix(a, k1)
     p2 = progogationMatrix(b, k2)
 
-    M1 = np.matmul(d21, p1)
-    M2 = np.matmul(d32, p2)
-    M =  np.matmul(M2, M1)
+    M1 = np.matmul(p1, d21)
+    M2 = np.matmul(p2, d32)
+    M =  np.matmul(M1, M2)
 
     if n == 1:
         return M
     else:
         return np.matmul(M,matrix_method(k1,k2,k3,n-1))
 
-def theory(e):
-    return 16*(e/v0)*math.exp((-2*a*math.sqrt(2*m*v0)/h**2)**2)
+# def func(e):
+#     k1 = cmath.sqrt(2*m*e/(h**2))
+#     k2 = cmath.sqrt((2*m*(e-v0))/(h**2))
+#     k3 = k1
+
+#     d21 = scatterinMatrix(k1, k2)
+#     d32 = scatterinMatrix(k2, k3)
+
+#     print(d21, d32)
+
+# func(0.3)
+
     
 for e in E:
     k1 = cmath.sqrt(2*m*e/(h**2))
@@ -91,7 +92,7 @@ for e in E:
     k3 = k1
 
     try:
-        M = matrix_method(k1,k2,k3,2)
+        M = matrix_method(k1,k2,k3,len(barriers))
         # print(M)
 
         r = -(M[1][0]/M[1][1])
@@ -103,16 +104,13 @@ for e in E:
         Rs.append(R)
         Ts.append(T)
 
-        T_th.append(theory(e))
-       
         total.append(T+R)
     except ZeroDivisionError:
          E = np.setdiff1d(E, np.array(e))
 
-# plt.plot(T_th,Ts , label='Theory')
-plt.plot(E, Rs, label="Reflection")
-plt.plot(E, Ts, label="Transmission")
-plt.plot(E, total)
+# plt.plot(Rs, E, label="Reflection")
+plt.plot(Ts, E, label="Transmission")
+# plt.plot(total, E)
 plt.ylabel('Energy')
 plt.title('Energy vs Transmission and Reflective Cofficients')
 plt.xlabel('R, T')
